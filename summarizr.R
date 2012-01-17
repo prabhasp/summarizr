90dd8dbe38de5ed2f16b3ab2e17c1474316e1669
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-01-17 11:44:07 -0500

    Pretty far along already, but first commit nevertheless

---
+++ summarizr.R
@@ -0,0 +1,79 @@
+library(gridExtra)
+library(ggplot2)
+library(plyr)
+source("~/Code/R/utils.R")
+
+########### PLOT UTILS ############
+# Tested filetypes, so far, are pdf and svg
+vecplot <- function(name_plot_prefix, filetype='svg') {
+	name = name_plot_prefix[[1]]
+	plot = name_plot_prefix[[2]]
+	prefix = name_plot_prefix[[3]]
+	fname = paste(prefix, name, filetype, sep='.')
+	ggsave(fname, plot=plot, height=2)
+	fname
+}
+noplot <- function(name_plot_prefix, filename_prefix='', suffix='') {
+	paste(name_plot_prefix[[3]], name_plot_prefix[[1]], suffix, sep='.')
+}
+
+oneplot <- function(name_plot_prefix_list_list, fname='output', filetype='svg', generate=TRUE) {
+	filenamestubs <- names(name_plot_prefix_list_list)
+	if (is.null(filenamestubs)) #unnamed list
+		filenames <- c(1:length(name_plot_prefix_list_list))
+	file_fn = if(filetype=='svg') {svg} else {pdf}
+	if (generate) {
+	    l_ply(filenames, function(fname) {
+	        plots_only = lapply(name_plot_prefix_list_list[[fname]], function(x) {x$plot})
+		fname = paste(fname, filetype, sep='.') # prefix.svg / prefix.pdf
+		file_fn(fname, height=(2*length(plots_only)))
+		do.call(grid.arrange, c(plots_only, ncol = 1))
+		dev.off()
+	     })
+	}
+	#todo: do this and earlier op together
+	unlist(lapply(filenames, function(fname) paste(fname, filetype, sep='.')))
+}
+	
+	
+	
+
+############# CORE STUFF -- just use this with plot utils ##################
+# take a one-column data frame, and plot it
+# returns NA if doesn't know how to plot, (name, plot) if it does
+ggraph_one <- function (one_column_df, prefix='') {
+	col_type <- class(one_column_df[[1]])
+	if(col_type=="integer") {
+		list(name = names(one_column_df)[[1]],
+			 plot = ggplot(data=one_column_df) + aes_string(x=names(one_column_df)[1]) + geom_histogram(),
+			 prefix = prefix)
+	} else if(col_type=="factor") {
+		list(name = names(one_column_df)[[1]],
+			plot = ggplot(data=one_column_df) + aes_string(fill=names(one_column_df)[1]) + aes(x=1)  + geom_bar(position="fill") + coord_flip() + opts(legend.position="bottom", legend.direction="horizontal", axis.title.x = theme_blank(), axis.title.y=theme_blank(), axis.text.y=theme_blank(), axis.ticks=theme_blank(), panel.grid.major=theme_blank(), panel.grid.minor=theme_blank()),
+			prefix = prefix)
+	} else {
+	    NA
+	}
+}
+# apply ggraph_one to a dataframe of many columns after sanification; drop NAs before returning; finally, return a one-element list
+ggraphs <- function(df, prefix='') {
+	df <- sanify_data_frame(df)	
+	temp_list <- lapply(names(df), function(colname) { ggraph_one(df[colname], prefix=prefix)	})
+	temp_list <- list(temp_list[which(!is.na(temp_list))])
+	names(temp_list <- lapply(temp_list, function(x) {x$name}))
+	temp_list
+}
+ggraphs_fname <- function(csvname, prefix='') { ggraphs(read.csv(csvname), prefix=prefix) }
+# apply graph to a dataframe, and aggreagate by factor_names
+ggraphs_with_agg <- function(df, factor_names) {
+	list_of_split_dfs = dlply(df, factor_names, function(x) {x[,!(names(x) %in% factor_names)]})
+	res_list <- lapply(names(list_of_split_dfs), function(name) {
+		unlist(ggraphs(list_of_split_dfs[[name]], prefix=name), recursive=FALSE)
+	})
+	names(res_list) <- names(list_of_split_dfs)
+	res_list
+}
+# same, except now this takes a filename rather than a dataframe
+ggraphs_with_agg_fname <- function(csvname, factor_names) {
+	ggraph_with_agg(read.csv(csvname), factor_names)
+}
---
+++ summarizr.py
@@ -0,0 +1,75 @@
+import cherrypy
+import rpy2.robjects as robjects
+import json, os
+
+class RGraphs(object):
+
+	def index(self):
+		return """
+        <html><body>
+            <h2>Upload a file</h2>
+            <form action="graph" method="post" enctype="multipart/form-data">
+	    Enter a google docs csv export url:
+		<input type="string" name="doc" /> <br/>
+	    Enter aggregation column name:
+		<input type="string" name="aggBy" /> <br/>
+		<input type="radio" name="generate" value="False" checked /> Just display images. 
+		<input type="radio" name="generate" value="True" /> Generate new images (takes a while). <br />
+            <input type="submit" />
+            </form>
+        </body></html>
+        """
+	index.exposed = True
+
+	def graph(self, doc, aggBy, generate):
+		robjects.r("library(RCurl)")
+		if doc == '':
+			df = robjects.r("read.csv(textConnection(getURL('https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AqkhmY48RtzudFBxUjR3NHVQdUZyQjFkODRyWV9wNGc&output=csv')))")
+		else: 
+			df = robjects.r("read.csv(textConnection(getURL('" + doc + "')))")
+		if aggBy in robjects.r['names'](df):
+			graphlist = self.graphs(df, aggBy, generate=="True")
+		else:
+			graphlist = self.graphs(df, None, generate=="True")
+		return "<html><body><ul><li>" + "</li><li>".join(graphlist) + "</li></ul></body></html>"
+	graph.exposed = True
+
+	def graphs(self, df, aggBy=None, generate=False):
+		#generate = True
+		robjects.r['source']('summarizr.R')
+		l = []
+		if aggBy:
+			l = robjects.r['ggraphs_with_agg'](df, aggBy)
+		else:
+			l = robjects.r['ggraphs'](df)
+		if generate:
+			old_d = robjects.r('getwd()')
+			robjects.r('setwd("' + os.path.join(os.getcwd(), "static") + '")')
+			filenames = robjects.r['oneplot'](l) 
+			robjects.r['setwd'](old_d)
+		else:
+			filenames = robjects.r['oneplot'](l, generate=False)
+		return map(lambda s:'<a href="static/' + s + '"><img height="100" width="400" src="static/' + s + '" / ></a>' + s + '<br/>', filenames)
+
+cherrypy.quickstart(RGraphs(), config=os.path.join(os.getcwd(), 'prod.conf'))
+
+# convert formhub json format to {"name" : "label"} dictionary
+def convert_to_name_label_dict(jsons):
+	# now we have a complicated list and dd architecture. What I want to do is keep delving in, and anytime
+	# we see a dict with name and label keys, we add to our dictionary
+	def f(arg, acc):
+		if (type(arg) == dict):
+			keys = arg.keys()
+			if "name" in keys and "label" in keys:
+				acc[arg["name"]] = arg["label"]
+			if "children" in keys:
+				return f(arg["children"], acc)
+			return acc
+		elif (type(arg) == list):
+			if len(arg)==0:
+				return acc
+			if type(arg[0])=="dict" or type(arg[0]=="list"):
+				acc = f(arg[0], acc)
+			return f(arg[1:], acc)
+	return f(json.loads(jsons), {})
+
