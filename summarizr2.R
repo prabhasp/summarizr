b3cb7c0bcc91166faeb45f19003a901ec823267d
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-03-04 01:57:36 -0500

    formatting changes in R; making the select-option-histogram into a stacked chart (need to figure out how to put things side by side now.

--- summarizr2.R
+++ summarizr2.R
@@ -1,8 +1,17 @@
 library(plyr)
 library(RJSONIO)
+library(RCurl)
 
-countdf <- function(df) { llply(df, function(x) { count(as.data.frame(x))}) }
+df_from_csv_url <- function(url) {
+    read.csv(textConnection(getURL(url))
+}
+
+countdf <- function(df) { 
+    llply(df, function(x) { count(as.data.frame(x))}) 
+}
 df_to_json_counts <- function(df, df_name) {
     toJSON(setNames(list(countdf(df)), list(df_name)))
 }
-df_to_json_counts_with_split <- function(df, splitBy) toJSON(dlply(df, splitBy, countdf)) 
+df_to_json_counts_with_split <- function(df, splitBy) {
+    toJSON(dlply(df, splitBy, countdf)) 
+}
